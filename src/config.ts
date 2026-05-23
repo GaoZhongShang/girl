fcc4dd61ec1c92f93d7dd282e825bd9e27ecd22e
Gzs <1705636606@qq.com>
2026-05-23 09:43:54 +0800

    chore: 更新 API 地址为 ECS 生产服务器 47.99.147.16

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
 // API 配置
-// Android 真机请使用电脑的局域网 IP，如 192.168.x.x
-// Android 模拟器请使用 10.0.2.2
-// iOS 模拟器请使用 localhost
-export const API_BASE_URL = 'http://192.168.11.239:5000';
+// ECS 生产服务器
+export const API_BASE_URL = 'http://47.99.147.16:5000';
